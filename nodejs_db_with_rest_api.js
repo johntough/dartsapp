eea97a08e1a19b96058444d1484e1449c1f6d93c
unknown <JOTO@DT001015.local.tmvse.com>
2015-06-24 10:28:11 +0100

    updatied packages.josn to work allow start from npm command

--- nodejs_db_with_rest_api.js
+++ nodejs_db_with_rest_api.js
@@ -97,4 +97,9 @@ require('./week')(restapi, db);
 
 restapi.use(express.static(__dirname + '/app'));
 
-restapi.listen(3000);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
+restapi.listen(server_port, server_ip_address, function(){
+    console.log("Listening on " + server_ip_address + ", port " + server_port);
+});
