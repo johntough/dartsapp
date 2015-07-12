1c282c03fb63ae4775d5846ee8df97666f4ecc2b
JOHN TOUGH <john.c.tough@gmail.com>
2015-07-12 19:29:37 +0100

    fixed twitter button

--- app/app.js
+++ app/app.js
@@ -561,6 +561,15 @@
     contactCtrl.contacts = [];
     contactCtrl.contactsLength = 0;
 
+    // Load twitter sharing button
+    (function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){
+            js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document, 'script', 'twitter-wjs'));
+
     contactCtrl.showPrivilegedData = function() {
         return loginService.isAuthorised();
     },
